e44c2fd12292339f2e421f089ea2819935e9a509
takeru-a <pypy20191022@gmail.com>
2024-10-19 11:28:15 +0900

    front-end環境構築

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from "node:path"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+  },
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./vitest-setup.ts"],
+  },
+})
